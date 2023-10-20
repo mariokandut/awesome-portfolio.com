0cbfa3bb6a08fc7218f47fbe2e48b140d25e92c1
Mario <mario.kandut@gmail.com>
2023-10-20 23:41:09 +0200

    fix: update graphQL queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   const result = await graphql(`
     {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
+      allMarkdownRemark(sort: {frontmatter: {date: DESC}}, limit: 1000) {
         edges {
           node {
             frontmatter {
@@ -23,6 +20,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       }
     }
   `);
+
 
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`);
